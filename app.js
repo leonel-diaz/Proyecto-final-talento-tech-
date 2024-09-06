6d92c8155a9efe530c31659d7f8d78b4c0fe0b8e
LeoDiaz <nemesis.94@hotmail.com>
2024-09-05 23:07:12 -0500

    registro y otros

--- app.js
+++ app.js
@@ -53,3 +53,16 @@ const cargarPeliculas = async() => {
 }
 
 cargarPeliculas();
+//logica registri
+function toggleForm() {
+	var loginForm = document.getElementById('login-form');
+	var registerForm = document.getElementById('register-form');
+	if (loginForm.style.display === 'none') {
+		loginForm.style.display = 'block';
+		registerForm.style.display = 'none';
+	} else {
+		loginForm.style.display = 'none';
+		registerForm.style.display = 'block';
+	}
+}
+
